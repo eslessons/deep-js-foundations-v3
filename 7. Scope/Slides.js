841425ff80af25f3db10e66f55aff5e0110d4f3d
eslessons <66363651+eslessons@users.noreply.github.com>
2020-08-17 14:06:43 +0300

    Finish 7. Scope

--- 7. Scope/Slides.js
+++ 7. Scope/Slides.js
@@ -1,1 +1,125 @@
 /* Scope
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Scope
+
+• Nested Scope
+• Hoisting
+• Closure
+• Modules
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Scope: where to look
+for things
+
+x = 42;
+console.log(y);
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Scope: sorting marbles
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Scope
+
+JavaScript organizes
+scopes with functions
+and blocks
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Scope
+
+var teacher = "Kyle";
+
+function otherClass() {
+  var teacher = "Suzy";
+  console.log("Welcome!");
+}
+
+function ask() {
+  var question = "Why?";
+  console.log(question);
+}
+
+otherClass(); // Welcome!
+ask();        // Why?
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Scope
+
+var teacher = "Kyle";
+
+function otherClass() {
+  teacher = "Suzy";
+  topic = "React";
+  console.log("Welcome!");
+}
+
+otherClass();       // Welcome!
+
+teacher;            // Suzy  // ???
+topic;              // React // ???
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Scope
+
+"use strict"
+
+var teacher = "Kyle";
+
+function otherClass() {
+  teacher = "Suzy";
+  topic = "React";  // ReferenceError
+  console.log("Welcome!");
+}
+
+otherClass();       
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Scope
+
+var teacher = "Kyle";
+
+function otherClass() {
+  teacher = "Suzy";
+  
+  function ask(question) {
+    console.log(teacher, question);
+  }
+
+  ask("Why?");
+}
+
+otherClass();   //  Suzy Why?
+ask("????");    // ReferenceError
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Scope
+
+undefined
+vs.
+undeclared
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Scope
+
+   Global Scope
+ ^   o o o o o             
+ |   o o o o o    Lexical 
+ |   o o o o o 
+ |   o o o o o    Scope(s)
+ |   o o o o o 
+   Current Scope
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+*/
